5953301f7b3db4c8578a37895fa2e86df3121c1b
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-29 16:13:35 +0200

    Debugging the fix.

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
+
 SRC_PATH=$(pwd)
 
 echo "SRC PATH: " ${SRC_PATH}
+echo "DEBUG: " $(ls -l ${SRC_PATH})
 
 docker run --rm -v ${SRC_PATH}:/usr/src/app-manager -w /usr/src/app-manager golang:1.4 /bin/bash -c 'go get -d && go build -v'
