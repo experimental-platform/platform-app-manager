e522ac763bbc71d042d6fb799344171692143248
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-29 16:05:54 +0200

    Debugging…

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
 
+echo "Current path: " $(pwd)
+
 docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/app-manager -w /usr/src/app-manager golang:1.4 /bin/bash -c 'go get -d && go build -v'
